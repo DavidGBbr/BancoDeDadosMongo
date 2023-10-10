52d5d576252f9f304cdf6414991d838a84fbd444
davidGBbr <davidgomesbrigido@gmail.com>
2023-10-10 15:05:13 -0300

    Exercício: Criar usuários novos

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,47 +1,8 @@
 import { Request, Response } from "express";
-
-import { Product } from "../models/Product";
 import User from "../models/User";
 
 export const home = async (req: Request, res: Response) => {
-  /*
-1ª forma:
+  let users = await User.find({}).sort({ "name.firstName": 1 });
 
-  let newUser = await User.create({
-    name: { firstName: "Monaliza", lastName: "Fernandes" },
-    email: "mona@paris.org",
-    age: 200,
-    interests: ["arte", "pizza"],
-  });
-
-  console.log("NOVO USUÁRIO", newUser);
-*/
-  //2ª forma:
-  let newUser = new User();
-  newUser.name = { firstName: "André", lastName: "Soares" };
-  newUser.email = "andre@hotmail.com";
-  newUser.age = 35;
-  newUser.interests = ["programação", "skate"];
-  let resultado = await newUser.save();
-
-  console.log("NOVO USUÁRIO", resultado);
-
-  let age: number = 90;
-  let showOld: boolean = false;
-
-  if (age > 50) {
-    showOld = true;
-  }
-
-  let list = Product.getAll();
-  let expensiveList = Product.getFromPriceAfter(12);
-
-  res.render("pages/home", {
-    name: "Bonieky",
-    lastName: "Lacerda",
-    showOld,
-    products: list,
-    expensives: expensiveList,
-    frasesDoDia: [],
-  });
+  res.render("pages/home", { users });
 };
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,32 +1,61 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
+import User from "../models/User";
 
 export const nome = (req: Request, res: Response) => {
-    let nome: string = req.query.nome as string;
-    let idade: string = req.query.idade as string;
+  let nome: string = req.query.nome as string;
+  let idade: string = req.query.idade as string;
 
-    res.render('pages/nome', {
-        nome,
-        idade
-    });
+  res.render("pages/nome", {
+    nome,
+    idade,
+  });
 };
 
 export const idadeForm = (req: Request, res: Response) => {
-    res.render('pages/idade');
+  res.render("pages/idade");
 };
 
 export const idadeAction = (req: Request, res: Response) => {
-    let mostrarIdade: boolean = false;
-    let idade: number = 0;
+  let mostrarIdade: boolean = false;
+  let idade: number = 0;
 
-    if(req.body.ano) {
-        let anoNascimento: number = parseInt(req.body.ano as string);
-        let anoAtual: number = new Date().getFullYear();
-        idade = anoAtual - anoNascimento;
-        mostrarIdade = true;
+  if (req.body.ano) {
+    let anoNascimento: number = parseInt(req.body.ano as string);
+    let anoAtual: number = new Date().getFullYear();
+    idade = anoAtual - anoNascimento;
+    mostrarIdade = true;
+  }
+
+  res.render("pages/idade", {
+    idade,
+    mostrarIdade,
+  });
+};
+
+export const addUserAction = async (req: Request, res: Response) => {
+  try {
+    const { firstName, lastName, email, age, interests } = req.body;
+
+    if (!firstName || !lastName || !email || !age || !interests) {
+      return res.status(400).send("Todos os campos são obrigatórios.");
     }
 
-    res.render('pages/idade', {
-        idade,
-        mostrarIdade
+    const newUser = new User({
+      name: { firstName, lastName },
+      email,
+      age: Number(age),
+      interests: interests
+        .split(",")
+        .map((interest: string) => interest.trim()),
     });
+
+    await newUser.save();
+
+    res.redirect("/");
+  } catch (error) {
+    console.error("Erro ao inserir novo usuário no banco de dados:", error);
+    res
+      .status(500)
+      .send("Ocorreu um erro ao inserir o usuário no banco de dados.");
+  }
 };
